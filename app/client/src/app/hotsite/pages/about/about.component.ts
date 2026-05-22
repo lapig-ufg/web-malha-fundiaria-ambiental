313ff1194467c5745bcbd3beda3d91a09cea3780
Tiago Gonçalves <tiagogonsalves2001@gmail.com>
2026-05-22 17:47:35 -0300

    update

--- app/client/src/app/hotsite/pages/about/about.component.ts
+++ app/client/src/app/hotsite/pages/about/about.component.ts
@@ -13,50 +13,49 @@ export class AboutCompenent {
       name: 'Laerte Guimarães Ferreira',
       image: 'assets/hotsite/images/team/laerte.jpeg',
       lattes: 'http://lattes.cnpq.br/',
-      role: 'Coordinator of the Pasture Research Program',
+      roleKey: 'hotsite.about.team.roles.laerte',
     },
     {
       name: 'Bernard Silva de Oliveira',
       image: 'assets/hotsite/images/team/bernard.jpeg',
       lattes: 'http://lattes.cnpq.br/',
-      role: 'PhD Candidate & Research Assistant',
+      roleKey: 'hotsite.about.team.roles.bernard',
     },
     {
       name: 'Ana Paula Matos e Silva',
       image: 'assets/hotsite/images/team/ana_silva.jpeg',
       lattes: 'http://lattes.cnpq.br/',
-      role: 'Doctoral Candidate & Research Assistant',
+      roleKey: 'hotsite.about.team.roles.ana_silva',
     },
     {
       name: 'Tharles de Sousa Andrade',
       image: 'assets/hotsite/images/team/tharles.jpeg',
       lattes: 'http://lattes.cnpq.br/',
-      role: 'Development Analyst',
+      roleKey: 'hotsite.about.team.roles.tharles',
     },
     {
       name: 'Felipe Jesus',
       image: 'assets/hotsite/images/team/felipe.jpeg',
       lattes: 'http://lattes.cnpq.br/',
-      role: 'Research Fellow in Geoprocessing and Remote Sensing',
+      roleKey: 'hotsite.about.team.roles.felipe',
     },
     {
       name: 'Guilherme Ramos Vaz',
       image: 'assets/hotsite/images/team/guilherme.jpeg',
       lattes: 'http://lattes.cnpq.br/',
-      role: 'Scientific Research Fellow in Environmental Analysis',
+      roleKey: 'hotsite.about.team.roles.guilherme',
     },
     {
       name: 'Ana Paula Carlos Assunção',
       image: 'assets/hotsite/images/team/ana_assuncao.jpeg',
       lattes: 'http://lattes.cnpq.br/',
-      role: 'Research fellow in Geoprocessing and Visual Interpretation',
+      roleKey: 'hotsite.about.team.roles.ana_assuncao',
     },
     {
       name: 'Tiago Goncalves Maia Geraldine',
       image: 'assets/hotsite/images/team/tiago.jpeg',
       lattes: 'http://lattes.cnpq.br/',
-      role: 'Development Analyst',
+      roleKey: 'hotsite.about.team.roles.tiago',
     },
-    
   ];
 }
